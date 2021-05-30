8d976091ff93139b1f723a7758c241a6f7084f67
Caleb Calvillo <39632290+Calvillo3@users.noreply.github.com>
2021-05-30 02:24:06 -0500

    still trying

--- public/test.js
+++ public/test.js
@@ -1,7 +1,9 @@
 //const fetch = require("node-fetch");
 
 
-fetch('https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=229EC1EF5800D26CDF74E448F2CCB67B&steamids=76561197960361544')
+fetch('https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=229EC1EF5800D26CDF74E448F2CCB67B&steamids=76561197960361544', {
+    mode: "no-cors"
+})
     .then(response => {response.json()})
     .then(data => {
         console.log(data);
