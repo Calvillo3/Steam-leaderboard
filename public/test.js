c794fdb82ae23cdc7820cfbb52ae9744c6d2eb75
Caleb Calvillo <39632290+Calvillo3@users.noreply.github.com>
2021-05-30 02:01:17 -0500

    key update

--- public/test.js
+++ public/test.js
@@ -1,7 +1,7 @@
 //const fetch = require("node-fetch");
 
 
-fetch('https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=A849BD363DA7A18C00AEAAE8415632F6&steamids=76561197960361544')
+fetch('https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=229EC1EF5800D26CDF74E448F2CCB67B&steamids=76561197960361544')
     .then(response => {response.json()})
     .then(data => {
         console.log(data);
