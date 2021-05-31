49b5db14b37b2930ad143b828dd5e0bf1c0661d6
Caleb Calvillo <39632290+Calvillo3@users.noreply.github.com>
2021-05-30 21:19:31 -0500

    get appid from title

--- routes.js
+++ routes.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const express = require("express");
+const appid = require("appid");
 
 const SteamAPI = require('steamapi');
 const steam = new SteamAPI('229EC1EF5800D26CDF74E448F2CCB67B');
@@ -11,8 +12,16 @@ const router = express.Router();
 
 router.route("/doc")
     .get((_req, res) => {
-        console.log("GET /");
+        // console.log("GET /");
         steam.getUserSummary('76561198146931523').then(summary => {
+            res.status(200).send(summary);
+        });
+    });
+
+router.route("/searchlist/:id")
+    .get((_req, res) => {
+        //console.log("GET /");
+        appid(new RegExp(_req.params.id, 'i')).then(summary => {
             res.status(200).send(summary);
         });
     });
